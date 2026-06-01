690f7e66dbb8945e8725ad6fe5cff38ece1161d1
snigdhads <snigdhads.007@gmail.com>
2026-06-01 18:32:13 +0530

    Cursor update

--- src/components/portfolio/Footer.jsx
+++ src/components/portfolio/Footer.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const Footer = () => (
   <footer className="footer">
     © {new Date().getFullYear()} D S Snigdha · Built with React
+    <p style={{ fontSize: "12px", marginTop: "20px" }}>
+  <a
+    href="https://www.flaticon.com/free-icons/pointer"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+  </a>
+</p>
   </footer>
 );
+
 
 export default Footer;
